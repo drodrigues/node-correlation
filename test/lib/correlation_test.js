223e7045a4a94dea2c5f67e503bb8c5f4a3984ef
Danilo Rodrigues <danilo.rodrigues@threadbrasil.com.br>
2016-01-06 16:04:57 -0200

    Small refactoring

--- test/lib/correlation_test.js
+++ test/lib/correlation_test.js
@@ -1,4 +1,4 @@
-var Correlation = require('./../../lib/correlation');
+var correlation = require('./../../lib/correlation');
 
 describe("Correlation", function() {
 
@@ -7,25 +7,25 @@ describe("Correlation", function() {
 
       it("raise an error when data is null", function() {
         expect(function() {
-          Correlation.calculate(null, null);
+          correlation(null, null);
         }).to.throw('Invalid values');
       });
 
       it("raise an error when data is not an array ", function() {
         expect(function() {
-          Correlation.calculate(1, 'b');
+          correlation(1, 'b');
         }).to.throw('Invalid values');
       });
 
       it("raise an error when data has any non-numeric value", function() {
         expect(function() {
-          Correlation.calculate([1, 'a'], [1, 2]);
+          correlation([1, 'a'], [1, 2]);
         }).to.throw('Invalid values');
       });
 
       it("raise an error when data have diferent size", function() {
         expect(function() {
-          Correlation.calculate([1, 3, 2], [1, 2]);
+          correlation([1, 3, 2], [1, 2]);
         }).to.throw('Invalid values');
       });
 
@@ -33,7 +33,7 @@ describe("Correlation", function() {
 
     context("when receive the valid data", function() {
       it("return the result", function() {
-        var result = Correlation.calculate([1, 2, 3, 4, 5], [0, 6, 2, 10, 4]);
+        var result = correlation([1, 2, 3, 4, 5], [0, 6, 2, 10, 4]);
 
         expect(result).to.equal(0.493196962)
       });
