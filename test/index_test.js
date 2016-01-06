045b3ff79c3d2e1f1192449c88a0c02296ca213d
Danilo Rodrigues <danilo.rodrigues@threadbrasil.com.br>
2016-01-06 16:26:31 -0200

    Small refactoring

--- test/index_test.js
+++ test/index_test.js
@@ -4,7 +4,7 @@ describe("Correlation", function() {
 
   context("version", function() {
     it('return the lib version', function() {
-      expect(correlation.version).to.equal('1.0.0')
+      expect(correlation.version).to.equal('1.0.2')
     });
   });
 
