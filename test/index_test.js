5220b6a24518ab49cf86bab426e1ea70aae749a0
Danilo Rodrigues <drodrigues@Danilos-MacBook-Pro.local>
2016-01-06 11:46:43 -0200

    Small refactoring

--- test/index_test.js
+++ test/index_test.js
@@ -1,9 +1,11 @@
 var correlation = require('../');
 
 describe("Correlation", function() {
+
   context("version", function() {
     it('return the lib version', function() {
       expect(correlation.version).to.equal('1.0.0')
     });
   });
+
 });
