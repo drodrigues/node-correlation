5692d1cd22b03ccf444a9c9d3833a9f8950dd8a1
Danilo Rodrigues <danilo@fillet.com.br>
2023-05-19 23:28:13 -0300

    Implemnted the correlation

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,7 +1,31 @@
-import { version } from './index';
+import { correlation } from './index';
 
-describe('Correlation', () => {
-  it('return the version', () => {
-    expect(version).toEqual('2.0.0');
+describe('correlation', () => {
+  describe('when receive the invalid data', function () {
+    it('raise an error when data is null', function () {
+      expect(() => correlation(null, null)).toThrowError('The values should be an array');
+    });
+
+    it('raise an error when data is not an array ', function () {
+      expect(() => correlation(1, 'b')).toThrowError('The values should be an array');
+    });
+
+    it('raise an error when data has any non-numeric value', function () {
+      expect(() => correlation([1, 'a'], [1, 2])).toThrowError('The all values should be numeric');
+    });
+
+    it('raise an error when data have diferent size', function () {
+      expect(() => correlation([1, 3, 2], [1, 2])).toThrowError(
+        'The arrays should have the same size'
+      );
+    });
+  });
+
+  describe('when receive the valid data', function () {
+    it('return the result', function () {
+      var result = correlation([1, 2, 3, 4, 5], [0, 6, 2, 10, 4]);
+
+      expect(result).toEqual(0.493196962);
+    });
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,41 @@
-export const version = '2.0.0';
+import isNumeric from 'fast-isnumeric';
+
+import { simple as simpleAverage } from './Average';
+import { standardDeviation } from './StandardDeviation';
+
+const isValidNumericArray = (values: number[]) => {
+  if (!(values instanceof Array)) throw TypeError('The values should be an array');
+
+  const hasNonNumericValue = values.find((value: number) => !isNumeric(value));
+
+  if (hasNonNumericValue) throw TypeError('The all values should be numeric');
+};
+
+export const correlation = (xValues: number[], yValues: number[]) => {
+  isValidNumericArray(xValues);
+  isValidNumericArray(yValues);
+
+  if (xValues.length !== yValues.length) throw TypeError('The arrays should have the same size');
+
+  const standardDeviationX = standardDeviation(xValues);
+  const averageX = simpleAverage(xValues);
+
+  const standardDeviationY = standardDeviation(yValues);
+  const averageY = simpleAverage(yValues);
+  const total = xValues.length;
+
+  let sumOfSquares = 0;
+
+  for (var index = 0, l = total; index < l; index++) {
+    const x = xValues[index];
+    const y = yValues[index];
+
+    sumOfSquares += (x - averageX) * (y - averageY);
+  }
+
+  const result = (
+    sumOfSquares /
+    ((xValues.length - 1) * standardDeviationX * standardDeviationY)
+  ).toFixed(9);
+  return parseFloat(result);
+};
